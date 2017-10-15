b7ca958f508b78822db3401918dc20310cf101be
GreyLabs <greylabs@protomail.com>
2017-10-16 02:10:33 +0500

    fix

--- hw.go
+++ hw.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
-
-	"google.golang.org/appengine"
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
@@ -20,17 +17,11 @@ func showInfo(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Inforamtion page for test project.\nLanguage - Go\nPlatform - Google Application Engine")
 }
 
-func stopServer(w http.ResponseWriter, r *http.Request) {
-	os.Exit(3)
-}
-
 func main() {
-	appengine.Main()
 	fmt.Println("Hello, test server started on 80 port.\n - /helloworld - show title page\n - /showinfo - show information about this thing")
 	http.HandleFunc("/", startPage)
 	http.HandleFunc("/helloworld", helloWorld)
 	http.HandleFunc("/showinfo", showInfo)
-	http.HandleFunc("/exit", stopServer)
 	http.ListenAndServe(":80", nil)
 }
 
