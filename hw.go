c7a109153251f5a06a45cd68f7fe762c19882e83
GreyLabs <greylabs@protomail.com>
2017-10-22 20:52:59 +0500

    action fix

--- hw.go
+++ hw.go
@@ -26,7 +26,8 @@ func startPage(w http.ResponseWriter, r *http.Request) {
 		templatePage.Execute(w, &webPage{"simplePage"})
 	case "POST":
 		r.ParseForm()
-		fmt.Fprintf(w, "Hello World!")
+		action := r.Form.Get("action")
+		fmt.Fprintf(w, "Successful read command/input from web-interface! Yeah! "+action)
 	}
 }
 
