8b6894b0ed32bb1419a5aaabc38711670a8115b4
GreyLabs <greylabs@protomail.com>
2017-10-29 16:56:05 +0500

    fix

--- hw.go
+++ hw.go
@@ -96,7 +96,6 @@ func statusServer(w http.ResponseWriter, r *http.Request) {
 	case "POST":
 		r.ParseForm()
 		fmt.Fprintf(w, "Get data by params in POST - OK")
-		fmt.Fprintf(w, string(bodyBytes))
 		statusContent = "POST request handled, " + "Node id: " + strings.Join(r.Form["nodeId"], " ") + ", Echo content: " + strings.Join(r.Form["echoContent"], " ")
 	}
 }
