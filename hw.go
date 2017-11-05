cf53c8e9126d9adca178631c4e9f2b416d37578f
GreyLabs <greylabs@protomail.com>
2017-11-06 01:31:37 +0500

    fix

--- hw.go
+++ hw.go
@@ -169,7 +169,7 @@ func logServer(w http.ResponseWriter, r *http.Request) {
 }
 
 func checkAliveStart(w http.ResponseWriter, r *http.Request) {
-	go periodicTask(500, checkIsAlive, 1, r)
+	periodicTask(500, checkIsAlive, 1, r)
 }
 
 /*
