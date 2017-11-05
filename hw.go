8c9da6b7988a4b4f717365ffb2d3454286478d51
GreyLabs <greylabs@protomail.com>
2017-11-06 00:44:17 +0500

    tst

--- hw.go
+++ hw.go
@@ -152,7 +152,7 @@ func checkIsAlive(nodeId int) {
 		statusLog += "Node #" + strconv.Itoa(nodeId) + " - offline"
 	}*/
 
-	ctx := BackgroundContext()
+	ctx := internal.BackgroundContext()
 	client := http.Client{Transport: &urlfetch.Transport{Context: ctx}}
 	resp, _ := client.Get(nodeUrl)
 	statusContent += string(resp.StatusCode)
